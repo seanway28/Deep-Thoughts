0b6f7dd77e463d936fa9a3772fa6e03156fde0dc
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-10-05 11:09:54 -0400

    src utils

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -19,12 +19,27 @@ class AuthService {
             } else {
                 return false;
             }
-            } catch (err) {
-                return false;
-            }
+        } catch (err) {
+            return false;
         }
+        
     }
 
     getToken() {
-        return localStorage.getItem('id_token')
+        return localStorage.getItem('id_token');
     }
+
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+
+        window.location.assign('/');
+    }
+}
+
+export default new AuthService();
